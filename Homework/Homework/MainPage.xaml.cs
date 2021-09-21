71e7fb018ea50cd3c2e615e26190bcef466ced02
Anis Shkembi <anis.shkembi@gmail.com>
2021-09-21 17:59:00 +0200

    Initial commit, 2 values able to be calculated

---
+++ Homework/Homework/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Homework
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void AddButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Result.Text =( int.Parse(Entry1.Text) + int.Parse(Entry2.Text)).ToString();
+        }
+    }
+}
