15530861f17cbad0141fee32a9054a52663555a4
Arber <arberbajraktari02@gmail.com>
2021-09-28 19:59:57 +0200

    Increment done with MVVM, trying to do the navigation

---
+++ Homework/Homework/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Homework.Views;
+using Xamarin.Forms;
+
+namespace Homework.ViewModels
+{
+    public class MainPageViewModel : NavigationPage
+    {
+
+        public MainPageViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+            ForwardButton = new Command(OnForwardAsync);
+        }
+
+        public ICommand ForwardButton { get; }
+        public ICommand IncreaseCount { get; }
+
+
+        int count = 0;
+        string countDisplay = "Click me!";
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+
+        private async void OnForwardAsync()
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+
+
+
+    }
+}
---
+++ Homework/Homework/Views/SecondPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework.ViewModels;
+using Xamarin.Forms;
+
+namespace Homework.Views
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
